63e784bf8f05501ab8ec6269128c398022f7c984
TurtleNaTree <kmcqueen108121@hotmail.com>
2022-05-29 11:16:44 +0000

    added code for the number buttons and updated the display css

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+
+initButtons();
+
+function initButtons(){
+    numInputs = document.getElementsByClassName("numInput");
+    for (const numButton of numInputs){
+        console.log(numButton.textContent);
+        numButton.addEventListener("click", () => updateDisp(numButton.textContent));
+    }
+}
+
 function add(a, b){
     return (a + b);
 }
@@ -30,4 +41,10 @@ function operate(operation, a, b){
             console.log("Something went wrong in operate");
             break;
     }
+}
+
+function updateDisp(value){
+    disp = document.getElementById("calcDisp");
+    disp.textContent = disp.textContent + value;
+    console.log(disp.textContent);
 }
